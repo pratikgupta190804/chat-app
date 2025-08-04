151945274f0c9bfafb0f72ada2655459e2febd36
pratikgupta190804 <pratikgupta040819@gmail.com>
2025-08-05 01:41:28 +0530

    A simple chat app is completed

---
+++ src/main/java/com/chat/ChatApp/chat/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.chat.ChatApp.chat;
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType type;
+}
---
+++ src/main/java/com/chat/ChatApp/chat/MessageType.java
@@ -0,0 +1,7 @@
+package com.chat.ChatApp.chat;
+
+public enum MessageType {
+    CHAT,
+    JOINED,
+    LEAVED
+}
